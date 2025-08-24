3760521ec8d68decb48d34b5dc9c01141d3e26fc
Erik Maier <rkmaier@gmail.com>
2025-08-24 10:42:53 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('/login', function () {
-    return response()->json(['message' => 'Unauthenticated.'], 401);
-})->name('login');
 
 
 
